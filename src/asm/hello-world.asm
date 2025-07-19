084854a203d52f470d3d7a4e9b2f054a7d3f9947
cliffhall <cliff@futurescale.com>
2025-07-19 10:51:07 -0400

    Add docs, fix program

--- src/asm/hello-world.asm
+++ src/asm/hello-world.asm
@@ -4,20 +4,18 @@
   .byte $9e, $32, $30, $36, $31, $00 ; SYS 2061 (start address)
   .word $0000 ; End of BASIC
 start:
-  lda #$00 ; Black background
+  lda $00 ; Black background
   sta $d020
   sta $d021
-  lda #$0f ; White text
-  sta $d020
-  lda #$01
+  lda $01 ; White text
   sta $0286
   jsr $e544 ; Clear screen
-  ldy #0 ; Index for string
-  ldx #$00 ; Initialize X for character color
+  ldy $0 ; Index for string
+  ldx $00 ; Initialize X for character color
 loop:
   lda message,y ; Load character from message
-  beq done ; End of string
-  jsr $ffd2 ; Print character (example subroutine)
+  beq done ; End of string (true if null terminator loaded)
+  jsr $ffd2 ; output character
   iny
   jmp loop
 done:
