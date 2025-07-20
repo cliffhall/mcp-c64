96a76382ced2eceecc52173facc5690d7541591d
cliffhall <cliff@futurescale.com>
2025-07-19 20:52:43 -0400

    * launch vice with a compiled program * constants in asm * WIP Something weird going on with lda

--- src/asm/hello-world.asm
+++ src/asm/hello-world.asm
@@ -11,22 +11,22 @@ CHROUT           = $ffd2 ; Output character
 RESET_OUTPUT     = $FFCC ; Reset output to screen
 SCREEN_COLOR     = $90   ; Black background
 TEXT_COLOR       = $05   ; White text
-CLRSCR           = $e544 ; Clear screen
+CLRSCR           = $FF81 ; Clear screen
 .enc "none"
 start:
-  ;jsr CLRSCR
-  lda SCREEN_COLOR
-  sta SET_BORDER_COLOR
-  sta SET_SCREEN_COLOR
-  lda TEXT_COLOR
-  sta SET_TEXT_COLOR
   jsr RESET_OUTPUT
+  jsr CLRSCR
+  clc
+;  lda SCREEN_COLOR
+;  sta SET_BORDER_COLOR
+;  sta SET_SCREEN_COLOR
+;  lda TEXT_COLOR
+;  sta SET_TEXT_COLOR
   ldy $0        ; Index for string
 loop:
   lda message,y ; Load next character from message
   beq done      ; End of string (true if null terminator loaded)
   jsr CHROUT    ; output character
-  rts
   iny           ; Increment index
   jmp loop      ; Repeat for next character
 done:
